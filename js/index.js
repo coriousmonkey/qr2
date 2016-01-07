8106e4e2974d387793c3ebf19b0064d547c81f79
harits wahyu <harits.wahyu.hid@gmail.com>
2016-01-07 15:19:12 +0700

    oke

--- js/index.js
+++ js/index.js
@@ -23,16 +23,18 @@ var app = {
     scan: function() {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        scanner.scan( function (result) { 
+        scanner.scan( function (result) 
+        { 
 			tulis(  
 				"We got a barcode\n" + 
 				"Result: " + result.text + "\n" + 
 				"Format: " + result.format + "\n" + 
-				"Cancelled: " + result.cancelled)
-			);
+				"Cancelled: " + result.cancelled
+            );
         }, function (error) { 
             alert("Scanning failed: ", error); 
-        } );
+        }
+        );
     },
 
     encode: function() {
--- js/index2.js
+++ js/index2.js
@@ -1,23 +1,26 @@
 $(document).ready(function(){
     $('#scan').on("tap",function(){
         scan();
-        
     });
     
     scan: function() {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-        scanner.scan( function (result) { 
+
+        scanner.scan( function (result) 
+        { 
 			tulis(  
 				"We got a barcode\n" + 
 				"Result: " + result.text + "\n" + 
 				"Format: " + result.format + "\n" + 
-				"Cancelled: " + result.cancelled)
-			);
+				"Cancelled: " + result.cancelled
+            );
         }, function (error) { 
             alert("Scanning failed: ", error); 
-        } );
+        }
+        );
     }
     function tulis(parameterr){
-        $("#info").html(parameterr);
-    }
-})
+    $("#info").html(parameterr);
+}
+    
+});
