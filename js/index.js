b23db940f7e5ea157ad279641a310beb0cb6fc06
harits wahyu <harits.wahyu.hid@gmail.com>
2016-01-07 14:00:55 +0700

    tes

---
+++ js/index.js
@@ -0,0 +1,52 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('scan').addEventListener('click', this.scan, false);
+        document.getElementById('encode').addEventListener('click', this.encode, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        tulis('ready');
+    },
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    },
+
+    scan: function() {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan( function (result) { 
+			tulis(  
+				"We got a barcode\n" + 
+				"Result: " + result.text + "\n" + 
+				"Format: " + result.format + "\n" + 
+				"Cancelled: " + result.cancelled)
+			);
+        }, function (error) { 
+            alert("Scanning failed: ", error); 
+        } );
+    },
+
+    encode: function() {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+
+    }
+};
+function tulis(parameterr){
+    $("#info").html(parameterr);
+}
---
+++ js/index2.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#scan').on("tap",function(){
+        scan();
+        
+    });
+    
+    scan: function() {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        scanner.scan( function (result) { 
+			tulis(  
+				"We got a barcode\n" + 
+				"Result: " + result.text + "\n" + 
+				"Format: " + result.format + "\n" + 
+				"Cancelled: " + result.cancelled)
+			);
+        }, function (error) { 
+            alert("Scanning failed: ", error); 
+        } );
+    }
+    function tulis(parameterr){
+        $("#info").html(parameterr);
+    }
+})
